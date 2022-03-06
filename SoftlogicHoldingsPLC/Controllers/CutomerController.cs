8264d8f1e9be8b39119f321f1f06e54d00a8351f
mirzanmiftha <gunitgstar@gmail.com>
2022-03-06 01:53:55 -0800

    Add files via upload
    Rest WEB-API

---
+++ SoftlogicHoldingsPLC/Controllers/CutomerController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SoftlogicHoldingsPLC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SoftlogicHoldingsPLC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CutomerController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public CutomerController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get() {
+            DataTable table = new DataTable();
+            try{
+                string query = @"SELECT * FROM dbo.Customer";
+                string sqlDataSource = _configuration.GetConnectionString("SoftlogicAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource)){
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon)){
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message); Console.WriteLine(ex.StackTrace); Console.WriteLine(ex.InnerException);
+
+            }
+        
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+        public JsonResult Post(Customer customer)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                string query = @" INSERT INTO dbo.Customer (name, phone, address, email) VALUES ( '" + customer.name + @"', '" + customer.phone + @"', '" + customer.address + @"', '" + customer.email + @"' ) ";
+                string sqlDataSource = _configuration.GetConnectionString("SoftlogicAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); Console.WriteLine(ex.StackTrace); Console.WriteLine(ex.InnerException);
+                return new JsonResult("Failed!.");
+
+            }
+
+            return new JsonResult("Add Successfully!.");
+        }
+
+        [HttpPut]
+        public JsonResult Put(Customer customer)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                string query = @" UPDATE dbo.Customer SET name = '" + customer.name + @"', phone = '" + customer.phone + @"', address = '" + customer.address + @"', email = '" + customer.email + @"' WHERE customer_id = " + customer.customer_id + @" ";
+                string sqlDataSource = _configuration.GetConnectionString("SoftlogicAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); Console.WriteLine(ex.StackTrace); Console.WriteLine(ex.InnerException);
+                return new JsonResult("Failed!.");
+
+            }
+
+            return new JsonResult("Updated Successfully!.");
+        }
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            DataTable table = new DataTable();
+            try
+            {
+                string ex_query = @" DELETE FROM dbo.Customer WHERE customer_id = "+ id + @"";
+                string sqlDataSource = _configuration.GetConnectionString("SoftlogicAppCon");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(ex_query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader);
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); Console.WriteLine(ex.StackTrace); Console.WriteLine(ex.InnerException);
+                return new JsonResult("Failed!.");
+
+            }
+
+            return new JsonResult("Deleted Successfully!.");
+        }
+
+
+   
+
+
+    }
+}
---
+++ SoftlogicHoldingsPLC/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftlogicHoldingsPLC.Models
+{
+    public class Customer
+    {
+        public int customer_id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string email { get; set; }
+    }
+}
